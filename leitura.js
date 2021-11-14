e6c8cecc492b34c1c00a18f987c4d938c3349858
Gean Gilberto Coimbra <geangilertocoimbra@gmail.com>
2021-11-13 22:10:22 -0300

    REPLACE

--- leitura.js
+++ leitura.js
@@ -9,7 +9,20 @@ const rl = readline.createInterface({
 
 rl.on('line', (line)=>{
     var resultado = line.toUpperCase();
-    // string = string.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
-    resultado = resultado.replace(/[^a-zA-Z]/g, ' ');
+    // console.log('>>>',resultado);
+    resultado = resultado.replace('Ã', 'A');
+    resultado = resultado.replace('À', 'A');
+    resultado = resultado.replace('Á', 'A');
+    resultado = resultado.replace('Â', 'A');
+    resultado = resultado.replace('Ç', 'C');
+    resultado = resultado.replace('Ê', 'E');
+    resultado = resultado.replace('É', 'E');
+    resultado = resultado.replace('Í', 'I');
+    resultado = resultado.replace('Ó', 'O');
+    resultado = resultado.replace('Õ', 'O');
+    resultado = resultado.replace('Ú', 'U');
+    // resultado = resultado.replace(/[^a-zA-Z]/g, ' ');
+    // resultado = resultado.split(' ');
     console.log('<<<',resultado);
 })
+// 
