bcc6aa6a1358ccd17db3b9c905d55079ca1b095f
Gean Gilberto Coimbra <geangilertocoimbra@gmail.com>
2021-11-15 22:37:19 -0300

    FILTER

--- leitura.js
+++ leitura.js
@@ -17,15 +17,16 @@ rl.on('line', (line)=>{
 
     resultado = acentuacao(resultado);
 
-    //TRATAMENTO NUMERICO
-    resultado = resultado.replace(/[0-9]/g, '');
-
     // TRATAMENTO DE CARACTERES ESPECIAIS E NUMERICOS
     resultado = resultado.replace(/[^a-zA-Z]/g, ' ');
 
+    //TRATAMENTO NUMERICO
+    resultado = resultado.replace(/[0-9]/g, ' ');
+
     //SEPARAÇÃO DAS PALAVRAS POR ESPAÇO
     resultado = resultado.split(' ');
-    console.log('<<<',resultado);
+    // console.log('<<<',resultado);
+    vetor(resultado);
 })
 
 function acentuacao(resultado){
@@ -46,58 +47,85 @@ function acentuacao(resultado){
 
 function tags(resultado){
     //TRATAMENTO DE TAGS
-    resultado = resultado.replace('<HTML>', '');
-    resultado = resultado.replace('<HEAD>', '');
-    resultado = resultado.replace('<TITLE>', '');
-    resultado = resultado.replace('ENTREVISTA Nº', '');
-    resultado = resultado.replace('</TITLE>', '');
-    resultado = resultado.replace('<LINK', '');
-    resultado = resultado.replace('REL=STYLESHEET', '');
-    resultado = resultado.replace('TYPE="TEXT/CSS"', '');
-    resultado = resultado.replace('HREF="ESTILOS.CSS"', '');
-    resultado = resultado.replace('</HEAD>', '');
-    resultado = resultado.replace('<BODY', '');
-    resultado = resultado.replace('BGCOLOR="#FFFFFF"', '');
-    resultado = resultado.replace('TEXT="#000000"', '');
-    resultado = resultado.replace('LINK="#009900"', '');
-    resultado = resultado.replace('VLINK="#000000"', '');
-    resultado = resultado.replace('ALINK="#000000"', '');
-    resultado = resultado.replace('LEFTMARGIN=', '');
-    resultado = resultado.replace('TOPMARGIN=', '');
-    resultado = resultado.replace('MARGINWIDTH=', '');
-    resultado = resultado.replace('MARGINHEIGHT=', '');
-    resultado = resultado.replace('<TABLE', '');
-    resultado = resultado.replace('CELLPADDING=', '');
-    resultado = resultado.replace('CELLSPACING=', '');
-    resultado = resultado.replace('BORDER=', '');
-    resultado = resultado.replace('<TR', '');
-    resultado = resultado.replace('VALIGN="TOP"', '');
-    resultado = resultado.replace('<TD', '');
-    resultado = resultado.replace('COLSPAN=', '');
-    resultado = resultado.replace('BGCOLOR="#FFCC66"', '');
-    resultado = resultado.replace('<FONT', '');
-    resultado = resultado.replace('SIZE="+1"', '');
-    resultado = resultado.replace('<B>', '');
-    resultado = resultado.replace('</B>', '');
-    resultado = resultado.replace('</FONT>', '');
-    resultado = resultado.replace('<P>', '');
-    resultado = resultado.replace('<A HREF="TABELA - CODIGOS DOS GRUPOS PROFISSIONAIS.HTM">', '');
-    resultado = resultado.replace('</A>', '');
-    resultado = resultado.replace('<BR>', '');
-    resultado = resultado.replace('<A HREF="TABELA - CODIGOS DOS INQUIRIDORES DO PF.HTM">', '');
-    resultado = resultado.replace('<A HREF="TABELA - CODIGOS DOS NIVEIS DE INSTRUCAO.HTM">', '');
-    resultado = resultado.replace('</TD>', '');
-    resultado = resultado.replace('</TR>', '');
-    resultado = resultado.replace('</TABLE>', '');
-    resultado = resultado.replace('WIDTH=', '');
-    resultado = resultado.replace('HEIGHT=', '');
-    resultado = resultado.replace('SIZE=', '');
-    resultado = resultado.replace('&NBSP;', '');
-    resultado = resultado.replace('FACE="COURIER NEW', '');
-    resultado = resultado.replace('</P>', '');
-    resultado = resultado.replace('A:', '');
-    resultado = resultado.replace('X:', '');
-    resultado = resultado.replace('</BODY>', '');
-    resultado = resultado.replace('</HTML>', '');
+    resultado = resultado.replace('<HTML>', ' ');
+    resultado = resultado.replace('<HEAD>', ' ');
+    resultado = resultado.replace('<TITLE>', ' ');
+    resultado = resultado.replace('ENTREVISTA Nº', ' ');
+    resultado = resultado.replace('</TITLE>', ' ');
+    resultado = resultado.replace('<LINK', ' ');
+    resultado = resultado.replace('REL=STYLESHEET', ' ');
+    resultado = resultado.replace('TYPE="TEXT/CSS"', ' ');
+    resultado = resultado.replace('HREF="ESTILOS.CSS"', ' ');
+    resultado = resultado.replace('</HEAD>', ' ');
+    resultado = resultado.replace('<BODY', ' ');
+    resultado = resultado.replace('BGCOLOR="#FFFFFF"', ' ');
+    resultado = resultado.replace('TEXT="#000000"', ' ');
+    resultado = resultado.replace('LINK="#009900"', ' ');
+    resultado = resultado.replace('VLINK="#000000"', ' ');
+    resultado = resultado.replace('ALINK="#000000"', ' ');
+    resultado = resultado.replace('LEFTMARGIN=', ' ');
+    resultado = resultado.replace('TOPMARGIN=', ' ');
+    resultado = resultado.replace('MARGINWIDTH=', ' ');
+    resultado = resultado.replace('MARGINHEIGHT=', ' ');
+    resultado = resultado.replace('<TABLE', ' ');
+    resultado = resultado.replace('CELLPADDING=', ' ');
+    resultado = resultado.replace('CELLSPACING=', ' ');
+    resultado = resultado.replace('BORDER=', ' ');
+    resultado = resultado.replace('<TR', ' ');
+    resultado = resultado.replace('VALIGN="TOP"', ' ');
+    resultado = resultado.replace('<TD', ' ');
+    resultado = resultado.replace('COLSPAN=', ' ');
+    resultado = resultado.replace('BGCOLOR="#FFCC66"', ' ');
+    resultado = resultado.replace('<FONT', ' ');
+    resultado = resultado.replace('SIZE="+1"', ' ');
+    resultado = resultado.replace('<B>', ' ');
+    resultado = resultado.replace('</B>', ' ');
+    resultado = resultado.replace('</FONT>', ' ');
+    resultado = resultado.replace('<P>', ' ');
+    resultado = resultado.replace('<A HREF="TABELA - CODIGOS DOS GRUPOS PROFISSIONAIS.HTM">', ' ');
+    resultado = resultado.replace('</A>', ' ');
+    resultado = resultado.replace('<BR>', ' ');
+    resultado = resultado.replace('<A HREF="TABELA - CODIGOS DOS INQUIRIDORES DO PF.HTM">', ' ');
+    resultado = resultado.replace('<A HREF="TABELA - CODIGOS DOS NIVEIS DE INSTRUCAO.HTM">', ' ');
+    resultado = resultado.replace('</TD>', ' ');
+    resultado = resultado.replace('</TR>', ' ');
+    resultado = resultado.replace('</TABLE>', ' ');
+    resultado = resultado.replace('WIDTH=', ' ');
+    resultado = resultado.replace('HEIGHT=', ' ');
+    resultado = resultado.replace('SIZE=', ' ');
+    resultado = resultado.replace('&NBSP;', ' ');
+    resultado = resultado.replace('FACE="COURIER NEW', ' ');
+    resultado = resultado.replace('</P>', ' ');
+    resultado = resultado.replace('A:', ' ');
+    resultado = resultado.replace('X:', ' ');
+    resultado = resultado.replace('</BODY>', ' ');
+    resultado = resultado.replace('</HTML>', ' ');
     return resultado;
 }
+
+//CRIAÇÃO DO VETOR
+function vetor(resultado){
+    texto.push(resultado.filter(Boolean));
+    for(var i = 0; i < 14; i++){
+        delete texto[i];
+    }
+    // exclusao();
+    console.log(texto);
+}
+
+function exclusao(){
+    var teste = texto.filter(Boolean);
+    console.log(teste);
+    
+}
+
+Array.prototype.clean = function() {
+    for (var i = 0; i < texto.length; i++) {
+        for (var j = 0; j < texto[i].length; j++) {
+            if (texto[i][j] === null) {         
+                texto[i].splice(j, 1);
+                j--;
+            }
+        }
+    }
+  };
